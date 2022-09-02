02d643e21af3e55223611e197b3fd6af1fe57f8b
King Anthony D. Retaga <kinganthonyretaga170@gmail.com>
2022-09-03 00:13:17 +0800

    feat: a simple navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-function App() {
+function One() {
+  const style = `
+    h2 {
+      color: crimson;
+    }
+  `;
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style>{style}</style>
+      <h2>Chapter 1</h2>
+    </>
   );
 }
 
-export default App;
+function Two() {
+  const style = `
+    h2 {
+      color: cadetblue;
+    }
+  `;
+
+  return (
+    <>
+      <style>{style}</style>
+      <h2>Chapter 2</h2>
+    </>
+  );
+}
+
+const CHAPTERS = [
+  {
+    number: 1,
+    title: 'Chapter 1',
+    Page: One
+  },
+  {
+    number: 2,
+    title: 'Chapter 2',
+    Page: Two
+  }
+];
+
+export default function App() {
+  const [currentNumber, setCurrentNumber] = useState(1);
+
+  const { title, Page } = CHAPTERS
+    .find(({ number }) => number === currentNumber);
+
+  useEffect(
+    () => {
+      document.title = title;
+    },
+    [currentNumber]
+  );
+
+  return (
+    <>
+      <nav>
+        <ul>
+          {CHAPTERS.map(({ number }) => (
+            <li key={number}>
+              <button onClick={() => setCurrentNumber(number)}>
+                Chapter {number}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Page/>
+    </>
+  );
+};
